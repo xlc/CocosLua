00f2ef8c0e40bba745790e4774b44855588cb429
Xiliang Chen <xlchen1291@gmail.com>
2012-05-17 21:05:40 +1200

    add document directory in search path

--- CocosLua/Scripts/init.lua
+++ CocosLua/Scripts/init.lua
@@ -1,4 +1,7 @@
 require 'cocos2d'
 require 'LuaScene'
 
+-- also search scripts in document directory
+package.path = package.path .. NSDocumentDirectory .. "/Scripts/?.lua;" .. NSDocumentDirectory .. "/Scripts/?/init.lua"
+
 print('loaded')
